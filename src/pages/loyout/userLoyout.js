023208c64919422fab36c4c55d04103a80455907
xk-shams <olimjonshamsiddinov04@gmail.com>
2025-03-27 16:03:48 +0500

    as

--- src/pages/loyout/userLoyout.js
+++ src/pages/loyout/userLoyout.js
@@ -1,11 +1,8 @@
 import React from "react";
-import Sidebar from "@/components/common/SideBar";
-import 
 
 function userLoyout() {
   return (
     <div style={{display: 'flex'}}>
-      <Sidebar />
       <div></div>
     </div>
   );
